bac56ea690d085cdf5b48e2120c07377e67c7c25
Franciscus Agnew <agnew_f@hotmail.com>
2018-07-26 15:44:30 -0700

    Add Player render method.

--- js/app.js
+++ js/app.js
@@ -26,6 +26,12 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
